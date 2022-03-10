a640f58dd19c35623b057b15d609004285b01b28
fantasticMan1 <joshua.winebrener@gmail.com>
2022-03-10 15:32:01 -0600

    add basic OpenCV livestream flask app

---
+++ livestream/app.py
@@ -0,0 +1,43 @@
+"""
+Made following [this guide](https://www.instructables.com/Video-Streaming-Web-Server/) and
+[this guide](https://towardsdatascience.com/video-streaming-in-web-browsers-with-opencv-flask-93a38846fe00)
+"""
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+
+CAMERA = None
+
+
+@app.route("/")
+def index():
+    """Video streaming home page."""
+    return render_template("index.html")
+
+
+def gen():
+    global CAMERA
+
+    """Video streaming generator function."""
+    if CAMERA is None:
+        CAMERA = cv2.VideoCapture(0)
+        if not CAMERA.isOpened():
+            raise RuntimeError("Could not open camera")
+    while True:
+        # frame = camera.get_frame()
+        _, img = CAMERA.read()
+        _, buf = cv2.imencode(".jpg", img)
+        frame = buf.tobytes()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/video_feed")
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
